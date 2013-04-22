70c267214979fbaf6de6fe0b38398921f54fcd0e
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-21 20:57:09 -0700

    Making main attempt to land once

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ from moonlander import Moonlander
 
 def main():
     moonlander = Moonlander()
+    while moonlander.landed == 'in_air':
+        moonlander.update()
+    print('Moonlander has {}'.format(moonlander.landed))
 
 
 if __name__ == '__main__':
