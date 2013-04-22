19bb20185dc2133ccfcb8dca32fc7d399b6e5f76
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-21 19:06:03 -0700

    Adding initial rule sets

--- fuzzy_control.py
+++ fuzzy_control.py
@@ -3,38 +3,79 @@ from fuzz import TrapezoidalFuzzyNumber as Trapezoid
 
 class FuzzyControl:
     def __init__(self):
-        self.vertical_sets = ['low', 'medium', 'high']
-        self.height_sets = [Trapezoid((0, 13), (0, 50)),
-                            Trapezoid((50, 63), (35, 75)),
-                            Trapezoid((75, 100), (55, 100))]
+        self.height_sets = {'low': Trapezoid((0, 13), (0, 50)),
+                            'medium': Trapezoid((50, 63), (35, 75)),
+                            'high': Trapezoid((75, 100), (55, 100))}
 
-        self.y_velocity_sets = [Trapezoid((0, 3), (0, 5)),
-                                Trapezoid((5, 60), (4, 90)),
-                                Trapezoid((90, 310), (50, 310))]
+        self.y_velocity_sets = {'safe': Trapezoid((0, 3), (0, 5)),
+                                'medium': Trapezoid((5, 60), (4, 90)),
+                                'fast': Trapezoid((90, 310), (50, 310))}
 
-        self.horizontal_sets = ['left', 'safe', 'right']
-        self.position_sets = [Trapezoid((-5, -0.3), (-5, -0.2)),
-                              Trapezoid((-0.1, 0.1), (-0.3, 0.3)),
-                              Trapezoid((0.3, 5), (0.2, 5))]
+        self.position_sets = {'left': Trapezoid((-5, -0.3), (-5, -0.2)),
+                              'safe': Trapezoid((-0.1, 0.1), (-0.3, 0.3)),
+                              'right': Trapezoid((0.3, 5), (0.2, 5))}
 
-        self.x_velocity_sets = [Trapezoid((-5, -2), (-5, -1)),
-                                Trapezoid((-0.5, 0.5), (-2, 2)),
-                                Trapezoid((2, 5), (1, 5))]
-        self.sets = [self.height_sets,
-                     self.y_velocity_sets,
-                     self.position_sets,
-                     self.x_velocity_sets]
+        self.x_velocity_sets = {'left': Trapezoid((-5, -2), (-5, -1)),
+                                'slow': Trapezoid((-0.5, 0.5), (-2, 2)),
+                                'right': Trapezoid((2, 5), (1, 5))}
+
+    def _get_burn(self, position, velocity):
+        rules = []
+        rules += min(self.height_sets['high'].mu(position),
+                     self.y_velocity_sets['fast'].mu(velocity))
+        rules += min(self.height_sets['high'].mu(position),
+                     self.y_velocity_sets['medium'].mu(velocity))
+        rules += min(self.height_sets['high'].mu(position),
+                     self.y_velocity_sets['slow'].mu(velocity))
+
+        rules += min(self.height_sets['medium'].mu(position),
+                     self.y_velocity_sets['fast'].mu(velocity))
+        rules += min(self.height_sets['medium'].mu(position),
+                     self.y_velocity_sets['medium'].mu(velocity))
+        rules += min(self.height_sets['medium'].mu(position),
+                     self.y_velocity_sets['slow'].mu(velocity))
+
+        rules += min(self.height_sets['low'].mu(position),
+                     self.y_velocity_sets['fast'].mu(velocity))
+        rules += min(self.height_sets['low'].mu(position),
+                     self.y_velocity_sets['medium'].mu(velocity))
+        rules += min(self.height_sets['low'].mu(position),
+                     self.y_velocity_sets['slow'].mu(velocity))
+
+        burn = (rules[0]*1, rules[1]*0, rules[2]*0,
+                rules[3]*2, rules[4]*1, rules[5]*0,
+                rules[6]*3, rules[7]*2, rules[8]*0) / sum(rules)
+        return burn
+
+    def _get_thrust(self, position, velocity):
+        rules = []
+        rules += min(self.position_sets['left'].mu(position),
+                     self.x_velocity_sets['left'].mu(velocity))
+        rules += min(self.position_sets['left'].mu(position),
+                     self.x_velocity_sets['slow'].mu(velocity))
+        rules += min(self.position_sets['left'].mu(position),
+                     self.x_velocity_sets['right'].mu(velocity))
+
+        rules += min(self.position_sets['safe'].mu(position),
+                     self.x_velocity_sets['left'].mu(velocity))
+        rules += min(self.position_sets['safe'].mu(position),
+                     self.x_velocity_sets['slow'].mu(velocity))
+        rules += min(self.position_sets['safe'].mu(position),
+                     self.x_velocity_sets['right'].mu(velocity))
+
+        rules += min(self.position_sets['right'].mu(position),
+                     self.x_velocity_sets['left'].mu(velocity))
+        rules += min(self.position_sets['right'].mu(position),
+                     self.x_velocity_sets['slow'].mu(velocity))
+        rules += min(self.position_sets['right'].mu(position),
+                     self.x_velocity_sets['right'].mu(velocity))
+
+        thrust = (rules[0]*0.3, rules[1]*0.2, rules[2]*0.1,
+                  rules[3]*0.1, rules[4]*0, rules[5]*-0.1,
+                  rules[6]*-0.3, rules[7]*-0.2, rules[8]*-0.1) / sum(rules)
+        return thrust
 
     def control_input(self, height, y_velocity, position, x_velocity):
-        burn, thrust = 0, 0
-        inputs = [height, y_velocity, position, x_velocity]
-        for i in range(len(inputs)):
-            sets = self.sets[i]
-            print('Input #{}'.format(i))
-            for j in range(len(sets)):
-                print(str(inputs[i]) +
-                      ': ' +
-                      self.vertical_sets[j] +
-                      ': ' +
-                      str(sets[j].mu(inputs[i])))
+        burn = self._get_burn(height, y_velocity)
+        thrust = self._get_thrust(height, y_velocity)
         return burn, thrust
