ec070b31cbc2f3cc83e1878a8548e4dd5a674ebb
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-22 13:17:14 -0700

    Acheiving ~ 30 percent success rate with wind

--- fuzzy_control.py
+++ fuzzy_control.py
@@ -13,14 +13,13 @@ class FuzzyControl:
                                 'medium': Trapezoid((9.0, 15.0), (4.0, 30.0)),
                                 'fast': Trapezoid((35.0, MAX), (25.0, MAX))}
 
-        self.position_sets = {'low': Trapezoid((-MAX, 1.6), (-MAX, 2.0)),
-                              'medium': Trapezoid((2.4, 2.4), (1.8, 3.0)),
-                              'high': Trapezoid((3.6, MAX), (3.0, MAX))}
+        self.position_sets = {'negative': Trapezoid((-MAX, -0.7), (-MAX, -0.2)),
+                              'zero': Trapezoid((-0.1, 0.1), (-0.2, 0.2)),
+                              'positive': Trapezoid((0.7, MAX), (0.2, MAX))}
 
-        self.x_velocity_sets = {'safe': Trapezoid((-MAX, 0.3), (-MAX, 0.5)),
-                                'medium': Trapezoid((0.5, 0.5), (0.3, 0.7)),
-                                'fast': Trapezoid((0.7, MAX), (0.5, MAX))}
-        self.wind_direction = None
+        self.x_velocity_sets = {'left': Trapezoid((-MAX, -0.6), (-MAX, -0.2)),
+                                'middle': Trapezoid((-0.2, 0.2), (-0.3, 0.3)),
+                                'right': Trapezoid((0.6, MAX), (0.2, MAX))}
 
     def _get_burn(self, position, velocity):
         rules = []
@@ -52,35 +51,35 @@ class FuzzyControl:
 
     def _get_thrust(self, position, velocity):
         rules = []
-        rules.append(min(self.position_sets['high'].mu(position),
-                         self.x_velocity_sets['fast'].mu(velocity)))
-        rules.append(min(self.position_sets['high'].mu(position),
-                         self.x_velocity_sets['medium'].mu(velocity)))
-        rules.append(min(self.position_sets['high'].mu(position),
-                         self.x_velocity_sets['safe'].mu(velocity)))
+        print(position, velocity)
+        rules.append(min(self.position_sets['negative'].mu(position),
+                         self.x_velocity_sets['left'].mu(velocity)))
+        rules.append(min(self.position_sets['negative'].mu(position),
+                         self.x_velocity_sets['middle'].mu(velocity)))
+        rules.append(min(self.position_sets['negative'].mu(position),
+                         self.x_velocity_sets['right'].mu(velocity)))
 
-        rules.append(min(self.position_sets['medium'].mu(position),
-                         self.x_velocity_sets['fast'].mu(velocity)))
-        rules.append(min(self.position_sets['medium'].mu(position),
-                         self.x_velocity_sets['medium'].mu(velocity)))
-        rules.append(min(self.position_sets['medium'].mu(position),
-                         self.x_velocity_sets['safe'].mu(velocity)))
+        rules.append(min(self.position_sets['zero'].mu(position),
+                         self.x_velocity_sets['left'].mu(velocity)))
+        rules.append(min(self.position_sets['zero'].mu(position),
+                         self.x_velocity_sets['middle'].mu(velocity)))
+        rules.append(min(self.position_sets['zero'].mu(position),
+                         self.x_velocity_sets['right'].mu(velocity)))
 
-        rules.append(min(self.position_sets['low'].mu(position),
-                         self.x_velocity_sets['fast'].mu(velocity)))
-        rules.append(min(self.position_sets['low'].mu(position),
-                         self.x_velocity_sets['medium'].mu(velocity)))
-        rules.append(min(self.position_sets['low'].mu(position),
-                         self.x_velocity_sets['safe'].mu(velocity)))
+        rules.append(min(self.position_sets['positive'].mu(position),
+                         self.x_velocity_sets['left'].mu(velocity)))
+        rules.append(min(self.position_sets['positive'].mu(position),
+                         self.x_velocity_sets['middle'].mu(velocity)))
+        rules.append(min(self.position_sets['positive'].mu(position),
+                         self.x_velocity_sets['right'].mu(velocity)))
+        print(rules)
 
-        thrust = sum((rules[0]*2.0, rules[1]*1.7, rules[2]*1.0,
-                      rules[3]*1.2, rules[4]*0.9, rules[5]*0.6,
-                      rules[6]*0.8, rules[7]*0.5, rules[8]*0.1))/sum(rules)
-        return self.wind_direction * thrust
+        thrust = sum((rules[0]*-1.0, rules[1]*-0.1, rules[2]*0.0,
+                      rules[3]*-0.2, rules[4]*0.0, rules[5]*0.2,
+                      rules[6]*0.0, rules[7]*0.1, rules[8]*1.0))/sum(rules)
+        return thrust
 
     def control_input(self, height, y_velocity, position, x_velocity, wind):
         burn = self._get_burn(height, y_velocity)
-        if self.wind_direction is None:
-            self.wind_direction = wind/wind
-        thrust = self._get_thrust(10*abs(position), 10*abs(x_velocity))
-        return burn, 0
+        thrust = self._get_thrust(10*position, 10*x_velocity)/10
+        return burn, thrust
