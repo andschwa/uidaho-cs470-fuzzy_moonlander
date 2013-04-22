e30c1b3979a5a56018c57ff6cf26d383ac6a7701
Andrew Schwartzmeyer <andrew.schwartzmeyer@gmail.com>
2013-04-22 13:16:35 -0700

    Adding average wind output to main

--- main.py
+++ main.py
@@ -7,13 +7,15 @@ from moonlander import Moonlander
 
 
 def main():
-    attempts = 100
+    attempts = 1000
     successes = 0.0
     final_position = 0.0
     final_y_velocity = 0.0
     final_fuel = 0.0
+    average_wind = 0.0
     for _ in range(attempts):
         moonlander = Moonlander()
+        average_wind += abs(moonlander.wind)
         while moonlander.landed == 'in_air':
             moonlander.update()
         if moonlander.landed == 'landed':
@@ -26,6 +28,7 @@ def main():
     print('Average final position: {}'.format(final_position/float(attempts)))
     print('Average final y_velocity: {}'.format(final_y_velocity/float(attempts)))
     print('Average final fuel: {}'.format(final_fuel/float(attempts)))
+    print('Average wind: {}'.format(average_wind/float(attempts)))
 
 
 if __name__ == '__main__':
